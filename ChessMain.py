0135a12489e5a0ab9fb09e1ebf37ba93e964c4ac
enes-turk <enesturk.nes@gmail.com>
2024-02-22 19:21:20 +0300

    feat1ChangesInTheGame

--- ChessMain.py
+++ ChessMain.py
@@ -22,10 +22,28 @@ def main():
     gs = ChessEngine.GameState()
     loadImages()
     running = True
+    sqSelected = () #no squares selected initially, keep track of the last click of the user (tuple: row, col)
+    playerClicks = [] #keep track of player clicks (two tuples: [(7, 4)])
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sqSelected == (row, col): #the user clicked the same square twice
+                    sqSelected = () # deselect
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)
+                if len(playerClicks) == 2: # after second click
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected = () # reset user clicks
+                    playerClicks = []
+                    
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
