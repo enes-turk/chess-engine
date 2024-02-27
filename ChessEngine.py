453e5af46dd7b497e8ec6132c21fee18acd3e641
Enes Türk <partuminfernum@gmail.com>
2024-02-27 17:43:47 +0300

    pawn moves

--- ChessEngine.py
+++ ChessEngine.py
@@ -42,7 +42,7 @@ class GameState():
         for r in range(len(self.board)): # number of rows
             for c in range(len(self.board[r])): # number of columns in a given row
                 turn = self.board[r][c][0]
-                if (turn == 'w' and self.WhiteToMove) and (turn == 'b' and not self.WhiteToMove):
+                if (turn == 'w' and self.WhiteToMove) or (turn == 'b' and not self.WhiteToMove):
                     piece = self.board[r][c][1]
                     if piece == 'p':
                         self.getPawnMoves(r, c, moves)
@@ -53,7 +53,28 @@ class GameState():
     get all the pawn moves for the pawn located at row, col and add the moves to the list
     '''
     def getPawnMoves(self, r, c, moves):
-        pass
+        if self.WhiteToMove:
+            if self.board[r-1][c] == "--":
+                moves.append(Move((r, c), (r-1, c), self.board))
+                if r == 6 and self.board[r-2][c] == "--":
+                    moves.append(Move((r,c), (r-2, c), self.board))
+            if c-1 >= 0: # captures to the left
+                if self.board[r-1][c-1][0] == 'b':
+                    moves.append(Move((r, c), (r-1, c-1), self.board))
+            if c+1 <= 7: # captures to the right
+                if self.board[r-1][c+1][0] == 'b':
+                    moves.append(Move((r, c), (r-1, c+1), self.board))   
+        else: # black pawn moves
+            if self.board[r+1][c] == "--":
+                moves.append(Move((r,c), (r+1, c), self.board))    
+                if r == 1 and self.board[r+2][c] == "--":
+                    moves.append(Move((r,c), (r+2, c), self.board))
+            if c-1 >= 0: # captures to the left
+                if self.board[r+1][c-1][0] == 'w':
+                    moves.append(Move((r,c), (r+1, c-1), self.board))
+            if c+1 <= 7: # captures to the right
+                if self.board[r+1][c+1][0] == 'w':
+                    moves.append(Move((r,c), (r+1, c+1), self.board))               
     
     def getRookMoves(self, r, c, moves):
         pass
@@ -75,7 +96,6 @@ class Move():
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
         self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
-        print(self.moveID)
     
     '''
     overriding the equals method
