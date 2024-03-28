975a54e383165bddccd6daf5a56367bf0333a54e
Enes Türk <partuminfernum@gmail.com>
2024-03-28 14:48:42 +0300

    finalized random_move_AI

--- ChessMain.py
+++ ChessMain.py
@@ -1,5 +1,5 @@
 import pygame as p
-import ChessEngine
+import ChessEngine, SmartMoveFinder
 
 WIDTH = HEIGHT = 512
 DIMENSION = 8
@@ -28,13 +28,16 @@ def main():
     sqSelected = () #no squares selected initially, keep track of the last click of the user (tuple: row, col)
     playerClicks = [] #keep track of player clicks (two tuples: [(7, 4)])
     gameOver = False
+    playerOne = True # If a human is playing white, then this will be True. If an AI playing, then this will be False
+    playerTwo = False # If a human is playing black, then this will be True. If an AI playing, then this will be False
     while running:
+        humanTurn = (gs.whiteToMove and playerOne) or (not gs.whiteToMove and playerTwo)
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
             # mouse handlers
             elif e.type == p.MOUSEBUTTONDOWN:
-                if not gameOver:
+                if not gameOver and humanTurn:
                     location = p.mouse.get_pos()
                     col = location[0]//SQ_SIZE
                     row = location[1]//SQ_SIZE
@@ -69,7 +72,16 @@ def main():
                     moveMade = False
                     animate = False
                     gameOver = False           
-                    
+        
+        # AI move finder logic
+        if not gameOver and not humanTurn:
+            AIMove = SmartMoveFinder.findRandomMove(validMoves)
+            gs.makeMove(AIMove)
+            moveMade = True
+            animate = True
+            
+        
+               
         if moveMade:
             if animate:
                 animateMove(gs.moveLog[-1], screen, gs.board, clock)
