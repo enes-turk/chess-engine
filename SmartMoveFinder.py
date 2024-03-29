b3f6e464ada5a7e77118cd19ba133d4567735182
Enes Türk <partuminfernum@gmail.com>
2024-03-29 16:28:14 +0300

    greedy AI and minmax without recursion AI

--- SmartMoveFinder.py
+++ SmartMoveFinder.py
@@ -1,5 +1,56 @@
 import random
 
+pieceScore = {'K': 0, 'Q': 9, 'R': 5, 'B': 3, 'N': 3, 'p': 1}
+CHECKMATE = 1000
+STALEMATE = 0
+
+
+
 def findRandomMove(validMoves):
     return validMoves[random.randint(0, len(validMoves)-1)]
-        
+
+
+'''
+Find the best move based on material
+'''
+
+def findBestMove(gs, validMoves):
+    turnMultiplier = 1 if gs.whiteToMove else -1
+    oppsMinMaxScore = CHECKMATE
+    bestPlayerMove = None
+    random.shuffle(validMoves)
+    for playerMove in validMoves:
+        gs.makeMove(playerMove)
+        oppsMoves = gs.getValidMoves()
+        oppsMaxScore = -CHECKMATE
+        for oppsMove in oppsMoves:
+            gs.makeMove(oppsMove)
+            if gs.checkmate:
+                score = -turnMultiplier * CHECKMATE
+            elif gs.stalemate:
+                score = STALEMATE
+            else:
+                score = -turnMultiplier * scoreMaterial(gs.board)
+            if score > oppsMaxScore:
+                oppsMaxScore = score
+            gs.undoMove()
+        if oppsMaxScore < oppsMinMaxScore:
+            oppsMinMaxScore = oppsMaxScore
+            bestPlayerMove = playerMove
+        gs.undoMove()
+
+    return bestPlayerMove
+
+'''
+Score the board based on material
+'''
+def scoreMaterial(board):
+    score = 0
+    for row in board:
+        for square in row:
+            if square[0] == 'w':
+                score += pieceScore[square[1]]
+            elif square[0] == 'b':
+                score -= pieceScore[square[1]]
+                
+    return score
